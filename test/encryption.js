684f226299c14daa359071ac57a8343b6e4a6b9e
Craig MacGregor <craigerm@gmail.com>
2012-11-29 16:45:42 -0500

    Added some unit tests for encryption

--- test/encryption.js
+++ test/encryption.js
@@ -1,9 +1,33 @@
 var encryption = require('../auth/encryption');
 
-describe('createHash()', function(){
-  it('should not be password', function(){    
+describe('encryption', function(){
+
+  describe('createHash()', function(){
+    it('should not return plain password', function(){    
+      var hash = encryption.hash('test123');
+      hash.should !== 'test123';
+      hash.length.should > 'test123'.length;
+    });
+
+    it('should generate different hashes', function(){
+      var hash1 = encryption.hash('test123');
+      var hash2 = encryption.hash('password');
+      hash1.should != hash2;
+    });
+  });
+
+  describe('validate()', function(){
     var hash = encryption.hash('test123');
-    hash.should !== 'test123';
-    hash.length.should > 'test123'.length;
+    var wrongHash = encryption.hash('asddsa');
+      
+    it('should not allow different hash', function(){
+      var result = encryption.validate(wrongHash, 'test123');
+      result.should === false;
+    });
+
+    it('should return true for correct hash and password', function(){
+      var result = encryption.validate(hash, 'test123');
+      result.should === true;
+    });
   });
 });
