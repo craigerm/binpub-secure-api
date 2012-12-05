497de1cb95718cbb0b07981496aca97afec949df
Craig MacGregor <craigerm@gmail.com>
2012-12-05 15:10:06 -0500

    Added back test for topic routes

--- test/topic-routes.js
+++ test/topic-routes.js
@@ -4,10 +4,26 @@ var server = require('../server')
 describe('topics routes', function() {
   describe('GET /v1/users/:username/repos/:reponame/topics', function() {
     it('should return 200 status', function(done) {
-      return done();
-   //   request(server)
-  //      .get('/v1/users/craigerm/repos/ysenate-info/topics')
-//        .expect(200, done);
+      request(server)
+        .get('/v1/users/craigerm/repos/ysenate-info/topics')
+        .expect(200, done);
     });
   });
+
+  describe('POST /v1/users/:username/repos/:reponame/topics', function() {
+    it('should be unauthorized if no access token', function(done) {
+      request(server)
+        .post('/v1/users/craigerm/repos/ysenate-info/topics')
+        .expect(401, done);
+    });
+  });
+
+  describe('GET /v1/users/:username/repos/:reponame/topics/topicid', function() {
+    it('should return 200', function(done) {
+      request(server)
+        .get('/v1/users/craigerm/repos/ysenate-info/topics/1')
+        .expect(200, done);
+    });
+});
+
 });
