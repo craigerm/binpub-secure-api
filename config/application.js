724360312b6237f5b8a9a17f2dcececb6fc951b1
Craig MacGregor <craigerm@gmail.com>
2012-12-07 15:22:57 -0500

    Fixed config environment

--- config/application.js
+++ config/application.js
@@ -15,11 +15,12 @@ var config = {
 
 // Environemnt might not be set. It nof we set it to development
 var environment = process.env.NODE_ENV || 'development';
+var configFile =  './config/' + environment + '.js';
 
 // Try and load the environment specific options and merge them to the config. 
 // We mainly do this for testing so we can have a "test" database that is wiped
 // clean.
-if(environment && fs.existsSync('./' + environment)) {
+if(environment && fs.existsSync(configFile)) {
   console.log('Loading %s config settings...', environment);
   var environmentConfig = require('./' + environment); 
   config = extend({}, config, environmentConfig);
