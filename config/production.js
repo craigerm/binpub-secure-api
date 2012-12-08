87a801ec92e78f4d8603a83917d9605ae7a1015a
Craig MacGregor <craigerm@gmail.com>
2012-12-07 20:54:23 -0500

    Added production environment settings

--- config/production.js
+++ config/production.js
@@ -1,5 +1,13 @@
 var config = {
-  authCallback: 'http://localhost:3333/auth-callback'
+  authCallback: 'http://localhost:3333/auth-callback',
+  port: 80,
+  url: process.env.BINPUB_API_URL, 
+  creds: {
+    redis_port: undefined, 
+    redis_host: process.env.REDISTOGO_URL,
+    mongoose_auth: process.env.MONGOLAB_URI
+  }
 };
+
 
 module.exports = config;
